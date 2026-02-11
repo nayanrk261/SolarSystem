ceaa8dd0a97c32684ad2d21bd41d0afd605e200e
Rhutul Naware <rhutulnaware31@gmail.com>
2026-02-11 19:55:41 +0530

    Updated dashboard UI and AR interactions

---
+++ Assets/DashboardController/Scripts/Readme.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class DashboardController : MonoBehaviour
+{
+    public GameObject dashboardUI;
+    public GameObject videoScreen;
+    public VideoPlayer videoPlayer;
+
+    // Called when "How Solar System Was Created" button is clicked
+    public void PlayCreationVideo()
+    {
+        dashboardUI.SetActive(false);
+        videoScreen.SetActive(true);
+        videoPlayer.Play();
+    }
+
+    // Called when "VR Tour" button is clicked
+    public void StartVRTour()
+    {
+        dashboardUI.SetActive(false);
+        // Later we will add camera movement here
+        Debug.Log("VR Tour Started");
+    }
+
+    // Called when "Quiz" button is clicked
+    public void OpenQuiz()
+    {
+        Debug.Log("Quiz clicked");
+    }
+}
